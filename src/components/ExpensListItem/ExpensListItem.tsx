6740e633e617a2e4bab354ef291c8717758f3bb2
Natalia Minenkova <nminiankova@gmail.com>
2022-11-16 18:21:47 +0300

    [proj_4] add components Badge&ExpensListItem

---
+++ src/components/ExpensListItem/ExpensListItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ExpensListItem = () => {
+  return (
+    <li>ExpensListItem</li>
+  )
+}
--- src/components/ExpensessList/ExpensessList.tsx
+++ src/components/ExpensessList/ExpensessList.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Badge } from '../Badge/badge'
+import { ExpensListItem } from '../ExpensListItem/ExpensListItem'
 
 export const ExpensessList = () => {
   return (
     <ul>
-     <li></li>
-     <li></li>
-     <li></li>
+   <ExpensListItem/>
+   <Badge/>
 
     </ul>
   )
