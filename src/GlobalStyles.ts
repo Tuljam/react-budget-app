8dd4f24b1d82a69143a9ebef4fe4d99759fe3660
Natalia Minenkova <nminiankova@gmail.com>
2022-11-16 16:16:38 +0300

    [proj_4] add GlobalStyles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{ margin: 0;
+  padding: 0;
+  box-sizing: border-box}
+#root {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+ body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  padding: 0;
+  margin: 0;
+  font-weight: normal;
+  font-style: normal;
+}
+a {
+  text-decoration: none;
+}
+`;
